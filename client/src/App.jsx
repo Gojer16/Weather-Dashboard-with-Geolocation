dc50509d1f19db4dab54dfb5395d5eaca5811e9e
Gojer <gojer@naver.com>
2025-05-15 20:31:05 -0400

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-2xl text-red-500 font-bold px-8 py-8'>Welcome to the React App</h1>
+      <h2 className='text-xl text-gray-500 font-semibold px-8 py-4'>Current Weather</h2>
     </>
   )
 }
