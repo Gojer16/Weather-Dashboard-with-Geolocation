656a0edd1526f1216dc13c2f9cec0ab5414d3be6
Gojer <gojer@naver.com>
2025-05-17 01:04:06 -0400

    MVP
    MVP

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,27 +13,29 @@ function App() {
   const { weather, loading: weatherLoading, apiError } = useWeather(enabled ? location:null);
 
   const isLoading = enabled && (geoLoading || weatherLoading)
-  if (isLoading) return <p>Loading...</p>
+  if (isLoading) return <p className='flex justify-center py-[30%] gap-4'><span className="loading loading-spinner text-primary"></span>Waiting...</p>
   if (geoError || apiError) return <p>Error: {geoError || apiError}</p>;
 
   return (
     <>
-      <h1 className='text-2xl text-red-500 font-bold '>Get the Current Weather Wherever You Are</h1>
-      <h2 className='text-xl text-gray-500 font-semibold '>Fast. Simple. Accurate.</h2>
+    <div className='grid-cols py-8 px-[20%] text-center items-center'>
+      
+        <h1 className='text-2xl text-red-500 font-bold mb-8'>Get the Current Weather Wherever You Are</h1>
+        <h2 className='text-xl text-gray-500 font-semibold mb-8'>Fast. Simple. Accurate.</h2>
+    
 
-      {!enabled && (
-        <button
-          onClick={() => setEnabled(true)}
-          className="px-6 py-2 bg-blue-600 text-white rounded shadow flex gap-2 items-center "
-        >
-        <GrMapLocation /> Get My Weather
-        </button>
-      )}
+        {!enabled && (
+        <button 
+        onClick={() => setEnabled(true)}
+        className="btn bg-gradient-to-r from-purple-600 to-indigo-600 active:scale-95 hover:scale-105 transition-transform duration-300  px-6 py-2  shadow-xl  text-white rounded gap-2 items-center">  <GrMapLocation /> Get My Weather</button>
+        )}
+      
+    </div>
 
       {isLoading && <p>Loading...</p>}
       {geoError && <p>Error: {geoError}</p>}
       {apiError && <p>Error: {apiError}</p>}
-
+      <div className=' flex justify-center text-center'>  
       {weather && (
         <div className="px-8 py-4">
           <h2>Weather in {weather.city}, {weather.country}</h2>
@@ -42,9 +44,10 @@ function App() {
           <p>Humidity: {weather.humidity}%</p>
           <p>Wind: {weather.wind} km/h</p>
           <p>Description: {weather.description}</p>
-          <p>Icon: {weather.icon}</p>
         </div>
+        
       )}
+      </div>
     </>
   )
 }
